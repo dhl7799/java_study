2598bcf242476b615d8b2ae6eb8ec65569af8dd3
이동현 <donghyundhl@naver.com>
2022-12-26 09:54:54 +0900

    Generic class, 가변 파라미터

--- practice04/src/main/java/prob01/Printer.java
+++ practice04/src/main/java/prob01/Printer.java
@@ -1,6 +1,7 @@
 package prob01;
 
 public class Printer {
+	/*
 	public void println(int i) {
 		System.out.println(i);
 	}
@@ -12,5 +13,25 @@ public class Printer {
 	}
 	public void println(boolean b) {
 		System.out.println(b);
+	}
+	*/
+	//extends랑 super로 제한
+	public <T> void println(T t) {
+		System.out.println(t);
+	}
+	
+	public <T> void println(T...ts) {
+		System.out.println(ts[0]);
+		for(T t : ts)
+			System.out.println(t);
+	}
+	
+	//가변 파라미터, 가변인자
+	public int sum(Integer...nums) {
+		int sum = 0;
+		for(Integer i : nums) {
+			sum += i;
+		}
+		return sum;
 	}
 }
