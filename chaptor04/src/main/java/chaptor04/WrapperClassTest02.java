d219c4fd0b1e063e29e77e1cc3f681a159eff115
이동현 <donghyundhl@naver.com>
2022-12-26 09:37:37 +0900

    Generic class

--- chaptor04/src/main/java/chaptor04/WrapperClassTest02.java
+++ chaptor04/src/main/java/chaptor04/WrapperClassTest02.java
@@ -23,6 +23,9 @@ public class WrapperClassTest02 {
 		//16진수
 		String sss = Integer.toHexString(15);
 		System.out.println(sss);
+		int[] data = {1,2,3,4,5};
+		int[] data2 = {1,1,1,1,1};
+		//int[data.length] tdata = data2;
 	}
 
 }
---
+++ chaptor04/src/main/java/chaptor04/note.java
@@ -0,0 +1,9 @@
+package chaptor04;
+
+public class note {
+
+	public static void main(String[] args) {
+		System.out.println(2^5^0^5);
+	}
+
+}
