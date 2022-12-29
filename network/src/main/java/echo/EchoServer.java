fe9b16253b16faf5343f31927ebbd3c695cebe9a
이동현 <donghyundhl@naver.com>
2022-12-29 13:16:49 +0900

    echo s/c final

--- network/src/main/java/echo/EchoServer.java
+++ network/src/main/java/echo/EchoServer.java
@@ -1,83 +1,76 @@
 package echo;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
 
-public class EchoServer extends Thread{
+public class EchoServer {
 	public static final int PORT = 8000;
+	
 	public static void main(String[] args) {
 		ServerSocket serverSocket = null;
+		
 		try {
-			
 			serverSocket = new ServerSocket();
+
+			serverSocket.bind(new InetSocketAddress("0.0.0.0", 8000), 10);
+			log("starts...[port:" + PORT + "]");
 			
-			
-			serverSocket.bind(new InetSocketAddress("0.0.0.0",PORT)); 
-			System.out.println("[server] starts....[port:" + PORT +"]");
-			
-			
-			Socket socket = serverSocket.accept(); 
-			
-			
+			Socket socket = serverSocket.accept();
+
+			InetSocketAddress inetRemoteSocketAddress = (InetSocketAddress)socket.getRemoteSocketAddress();
+			String remoteHostAddress = inetRemoteSocketAddress.getAddress().getHostAddress();
+			int remotePort = inetRemoteSocketAddress.getPort();
+			log("connected by client[" + remoteHostAddress + ":" + remotePort + "]");
 			
 			try {
-					InetSocketAddress inetRemoteSocketAddress = (InetSocketAddress) socket.getRemoteSocketAddress(); //다운캐스트
-					
-					String remoteHostAddress = inetRemoteSocketAddress.getAddress().getHostAddress();
-					int remotePort = inetRemoteSocketAddress.getPort();
-					System.out.println("[server] connected by client[ " + remoteHostAddress + ", " + remotePort +" ]");
-					
-					
-					OutputStream os = socket.getOutputStream();
-					InputStream is = socket.getInputStream();
-					
-					
-					while(true) {
-						byte[] buffer = new byte[256];
-						
-						int readbytecount = is.read(buffer); //blocking
-						if(readbytecount == -1) {
-							System.out.println("[server] closed by client");
-							break;
-						}
-						String data = new String(buffer,0,readbytecount,"UTF-8");
-						System.out.println("[server] received: " + data);
-						
-						os.write(data.getBytes("UTF-8"));
+				PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "utf-8"), true);
+				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "utf-8"));
+				
+				while(true) {
+					String data = br.readLine();
+					if(data == null) {
+						log("closed by client");
+						break;
 					}
 					
-					
-					
-					
-				} catch (SocketException ex) {
-					// TODO Auto-generated catch block
-					System.out.println("[server] suddenly closed by client");
-				}catch(IOException ex) {
-					System.out.println("[server] error: " + ex);
-				} finally {
-					try {
-						if(socket != null && !socket.isClosed())
-							socket.close();
-					} catch(IOException ex) {
-						ex.printStackTrace();
-					}
+					log("received:" + data);
+					pw.println(data);
 				}
+			} catch(SocketException ex) {
+				System.out.println("[server] suddenly closed by client");
+			} catch(IOException ex) {
+				log("error:" + ex);
+			} finally {
+				try {
+					if(socket != null && !socket.isClosed()) {
+						socket.close();
+					}
+				} catch(IOException ex) {
+					ex.printStackTrace();
+				}
+			}
 		} catch (IOException e) {
-			System.out.println("[server] error: " + e);
+			log("error:" + e);
 		} finally {
 			try {
-				if(serverSocket != null && !serverSocket.isClosed())
+				if(serverSocket != null && !serverSocket.isClosed()) {
 					serverSocket.close();
+				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-
+	}
+	
+	private static void log(String message) {
+		System.out.println("[EchoServer] " + message);
 	}
 
 }
